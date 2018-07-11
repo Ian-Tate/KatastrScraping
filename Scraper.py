40765f1e819af6471cc5cda135c8976c01a2b1a0
JanHome <jan.henek@gmail.com>
2018-07-11 07:27:07 +0200

    Not the best, but smthng

--- Scraper.py
+++ Scraper.py
@@ -1,12 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
+def fill_first_form(self, insert):
+    payload = {'ctl00$bodyPlaceHolder$vyberObecKU$vyberKU$txtKU': insert}
+    r = requests.get(self, params=payload)
+    print (r.status_code)
+    #with open("requests_results.html", "wb") as f:
+    #    f.write(r.content)
+    return r
+
 
 def scrap_seznam_nemovitosti(self):
     r = requests.get(self)
     print(r)
     soup = BeautifulSoup(r.text, "html.parser")
-    #print("************************** ZDE *****************************")
     table = soup.findAll(class_="zarovnat stinuj ")  # MÁM TĚ !!! TOHLE VYPÍŠE TU SPRÁVNOU TABULKU
     #print(table)
     return table
+
+
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import Scraper
+import requests
+import webbrowser
+from bs4 import BeautifulSoup
 
-url = "http://nahlizenidokn.cuzk.cz/ZobrazObjekt.aspx?encrypted=TpAPYChmh9dJYLP1ld3eLnekI72grEUiRKbm4ltAxuR6Dc7OFq_BhT9_uz1MAXZ3wmCXL-P46sXgAN1tu3jE8YBxpn6sMEQOOKI3rlfX5OQOWNamowDing=="
+url = "http://nahlizenidokn.cuzk.cz/VyberLV.aspx"
+url1 = "http://nahlizenidokn.cuzk.cz/ZobrazObjekt.aspx?encrypted=TpAPYChmh9dJYLP1ld3eLnekI72grEUiRKbm4ltAxuR6Dc7OFq_BhT9_uz1MAXZ3wmCXL-P46sXgAN1tu3jE8YBxpn6sMEQOOKI3rlfX5OQOWNamowDing=="
 
-table = Scraper.scrap_seznam_nemovitosti(url)
+payloadOne = {'ctl00$bodyPlaceHolder$vyberObecKU$vyberKU$txtKU': 600016}
+payloadTwo = {'ctl00$bodyPlaceHolder$txtLV': 1}
+s = requests.Session()
+r = s.post(url, params=payloadOne)
+print(r.status_code)
+
+ra = s.post(url, params=payloadTwo)
+print(ra.status_code)
+
+
+
+table = Scraper.scrap_seznam_nemovitosti(url1)
 print(table)
